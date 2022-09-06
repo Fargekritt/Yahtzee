e8f4b6ce21c8051db84a51ffd751ed92d83bbb8e
Rikatob <89411878+Rikatob@users.noreply.github.com>
2022-09-06 13:48:46 +0200

    🔵 shouldReturnAmountOf1s

--- src/main/java/org/fredrik/lesson2/yahtzee/DiceHand.java
+++ src/main/java/org/fredrik/lesson2/yahtzee/DiceHand.java
@@ -1,6 +1,21 @@
 package org.fredrik.lesson2.yahtzee;
 
 public class DiceHand {
-    public DiceHand(Dice[] diceHand) {
+    private Dice[] dices;
+
+    public DiceHand(Dice[] dices) {
+        this.dices = dices;
+    }
+
+    public int getDiceAmount(Dice diceValue) {
+        int amount = 0;
+
+        for (Dice dice : dices) {
+
+            if (dice.compareTo(diceValue) == 0) {
+                amount++;
+            }
+        }
+        return amount;
     }
 }
--- src/main/java/org/fredrik/lesson2/yahtzee/Yahtzee.java
+++ src/main/java/org/fredrik/lesson2/yahtzee/Yahtzee.java
@@ -1,10 +1,15 @@
 package org.fredrik.lesson2.yahtzee;
 
 public class Yahtzee {
+
+    private DiceHand diceHand;
+
     public Yahtzee(DiceHand diceHand) {
+        this.diceHand = diceHand;
     }
 
     public int CountDice(Dice diceValue) {
-        return 2;
+        return diceHand.getDiceAmount(diceValue);
+
     }
 }
